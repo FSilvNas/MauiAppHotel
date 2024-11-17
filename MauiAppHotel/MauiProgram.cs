b12947ee8c7e4ee5c31cbf4469e81576daf0f344
FSilvNas <fsilvnas@gmail.com>
2024-11-17 03:59:06 -0300

    Envio de alterações Agenda 13 - Desenvolvimento de sistemas

--- MauiAppHotel/MauiProgram.cs
+++ MauiAppHotel/MauiProgram.cs
@@ -13,6 +13,10 @@ namespace MauiAppHotel
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Montserrat-Italic-VariableFont_wght.ttf", "MontserratItalicVariableFontwght");
+                    fonts.AddFont("Montserrat-VariableFont_wght.ttf", "MontserratVariableFontwght");
+
+
                 });
 
 #if DEBUG
