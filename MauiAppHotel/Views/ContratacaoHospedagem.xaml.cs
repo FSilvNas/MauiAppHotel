8d92d7a2653e64a48a3bca3674193a723c8ecb1e
FSilvNas <fsilvnas@gmail.com>
2024-11-17 15:31:12 -0300

    Configuração de datas de check-in e check-out,  configuração de lista suspensa

--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -2,9 +2,24 @@ namespace MauiAppHotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
 {
+
+	App PropriedadesApp;
+
+
 	public ContratacaoHospedagem()
 	{
 		InitializeComponent();
+
+		PropriedadesApp = (App)Application.Current;
+
+		pck_quarto.ItemsSource = PropriedadesApp.lista_quartos;
+
+		dtpck_checkin.MinimumDate = DateTime.Now;
+		dtpck_checkin.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month +1, DateTime.Now.Day);
+
+		dtpck_out.MinimumDate = dtpck_checkin.Date.AddDays(1);
+		dtpck_out.MaximumDate = dtpck_checkin.Date.AddMonths(6);
+	
 	}
 
 	private async void SobreButton_Clicked(object sender, EventArgs e)
@@ -22,6 +37,17 @@ public partial class ContratacaoHospedagem : ContentPage
 		{
 			DisplayAlert("Ops", ex.Message, "Ok");
 		}
+
+    }
+
+    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+		DatePicker elemento = sender as DatePicker;
+
+		DateTime data_selecionada_checkin = elemento.Date;
+
+		dtpck_out.MinimumDate = data_selecionada_checkin.AddDays(1);
+		dtpck_out.MaximumDate = data_selecionada_checkin.AddMonths(6);
 
     }
 }
