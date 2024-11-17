5dc371fac68da6929591b368ffab53f3354e71e3
FSilvNas <fsilvnas@gmail.com>
2024-11-17 14:13:33 -0300

    Adição da função do ButtonAvançar da pagina Inicial

--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -11,4 +11,17 @@ public partial class ContratacaoHospedagem : ContentPage
 	{
 		await Navigation.PushAsync(new Sobre());
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Navigation.PushAsync(new HospedagemContratada());
+
+		} catch (Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "Ok");
+		}
+
+    }
 }
